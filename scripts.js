19b388b345bdd5de142f770a0313db5876b85eb5
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-02-19 15:51:31 +0600

    Added fetch firebases

--- scripts.js
+++ scripts.js
@@ -2,6 +2,29 @@ let button = document.querySelector('button');
 let inputName = document.querySelector('#name');
 let inputMessage = document.querySelector('#message');
 let inputColor = document.querySelector('#color');
-button.addEventListener('click', function(){
-    console.log(inputName.value , inputColor.value, inputMessage.value);
-})
+
+
+
+button.addEventListener('click', function () {
+    let div = document.createElement('div');
+    let spanName = document.createElement('span');
+    let spanMessage = document.createElement('span');
+
+    spanName.innerText = inputName.value + ": ";
+    spanMessage.innerText = inputMessage.value;
+    spanName.style.color = inputColor.value;
+    spanName.style.fontWeight = 'bold';
+
+    div.append(spanName, spanMessage);
+    document.body.append(div);
+
+    fetch("./https://live-chat-e82e1-default-rtdb.firebaseio.com/chat.json" , {
+        method: "POST",
+        body: JSON.stringify({
+            name: inputName.value,
+            message: inputMessage.value,
+            color: inputColor.value,
+        })
+    })
+});
+
